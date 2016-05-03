8d20bd6dd9febfe07ce206ce3ad3d343833bd54a
milan94 <milan.vucenovic@yahoo.com>
2016-05-03 19:00:08 +0200

    first commit
    App with empty activities

---
+++ app/src/main/java/com/example/milan/teamproject/ArticleView.java
@@ -0,0 +1,13 @@
+package com.example.milan.teamproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ArticleView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article_view);
+    }
+}
